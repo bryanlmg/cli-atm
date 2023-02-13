de3649c7ed12c6bba9d0b1f0bda1b7dc7b4f1a15
bryan <bryanlmorales@gmail.com>
2023-02-13 12:07:00 -0500

    Fix methods

--- src/Main.java
+++ src/Main.java
@@ -3,38 +3,31 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main {
-    // ANSI color escape codes
+    // ANSI color escape codes for colorful output to console
     private static final String ANSI_RESET = "\u001B[0m";
     private static final String ANSI_RED = "\u001B[31m";
     private static final String ANSI_GREEN = "\u001B[32m";
     private static final String ANSI_YELLOW = "\u001B[33m";
-    private static final String ANSI_BLUE = "\u001B[34m";
 
     public static void main(String[] args) {
+        // maximum number of accounts allowed
         final int MAX_ACCOUNTS = 15;
-        int totalNumOfAccounts;
-        char choice;
-        Scanner scanner = new Scanner(System.in);
 
-        // instantiate parallel arrays with data from file
+        // parallel arrays that will hold account data
         int[] accountNumbers = new int[MAX_ACCOUNTS];
         double[] accountBalances = new double[MAX_ACCOUNTS];
-        totalNumOfAccounts = readBankAccounts(accountNumbers, accountBalances);
 
-        System.out.println(ANSI_GREEN + "Successfully read in " + totalNumOfAccounts + " accounts.");
-        System.out.println("Now printing the initial customer database..."
-                         + ANSI_RESET);
-
+        int totalNumOfAccounts = readBankAccounts(accountNumbers, accountBalances);
+        System.out.println(ANSI_GREEN + "Successfully read in " + totalNumOfAccounts + " accounts!");
+        System.out.println("Now printing the initial customer database..." + ANSI_RESET);
         printCustomerDatabaseTable(accountNumbers, accountBalances, totalNumOfAccounts);
 
-        // printing main menu
-        System.out.println();
-        System.out.println(ANSI_YELLOW + "Please select an option from the main menu below.\n"
-                        + ANSI_RESET);
+        System.out.println(ANSI_YELLOW + "\nPlease select an option from the main menu below.\n" + ANSI_RESET);
         printMainMenu();
-        System.out.println();
 
-        // getting user input
+        // get user input and evaluate it
+        Scanner scanner = new Scanner(System.in);
+        char choice;
         do {
             choice = scanner.next().charAt(0);
             switch (choice) {
@@ -56,7 +49,7 @@ public class Main {
 
                 case 'B':
                 case 'b':
-                    showAccountBalance(accountNumbers, accountBalances, totalNumOfAccounts, scanner);
+                    getAccountBalance(accountNumbers, accountBalances, totalNumOfAccounts, scanner);
                     break;
 
                 case 'X':
@@ -70,7 +63,7 @@ public class Main {
                     break;
 
                 default:
-                    System.out.println(ANSI_RED + "Incorrect option." + ANSI_RESET);
+                    System.out.println(ANSI_RED + "Not a valid option." + ANSI_RESET);
             }
 
             if (choice != 'q' && choice != 'Q') {
@@ -80,10 +73,6 @@ public class Main {
             }
         } while(choice != 'q' && choice != 'Q');
 
-
-        System.out.println(ANSI_YELLOW + "Printing the updated customer database..." + ANSI_RESET);
-        printCustomerDatabaseTable(accountNumbers, accountBalances, totalNumOfAccounts);
-        System.out.println(ANSI_YELLOW + "New total number of accounts: " + totalNumOfAccounts);
     }
 
     /**
@@ -91,7 +80,7 @@ public class Main {
      */
     private static void printMainMenu() {
         System.out.println(ANSI_YELLOW +
-                           "\tATM MENU");
+                           "\tMAIN MENU");
         System.out.println("\t========" +
                            ANSI_RESET);
         System.out.println(ANSI_GREEN +
@@ -118,7 +107,7 @@ public class Main {
         int counter = 0;
 
         try {
-            Scanner scanner = new Scanner(new File("/home/bryan/Dev/cli-atm/src/database.txt"));
+            Scanner scanner = new Scanner(new File("database.txt"));
             while (scanner.hasNextInt() && scanner.hasNextDouble()) {
                 accountNumbers[counter] = scanner.nextInt();
                 accountBalances[counter] = scanner.nextDouble();
@@ -129,24 +118,42 @@ public class Main {
             System.out.println("FileNotFoundException thrown.");
             e.printStackTrace();
         }
-
         return totalAccountsRead;
     }
 
     /**
      * Searches linearly for an account number in the accountNumbers array.
      * @param accountNumbers An integer array containing customer account numbers.
-     * @param numOfAccounts Total number of successful accounts read in.
-     * @param accountNumber The account number being sought.
+     * @param numOfAccounts  Total number of successful accounts read in.
      * @return The index of the account in the accountNumbers array if it exists, else -1 if it does not.
      */
-    private static int findAccount(int[] accountNumbers, int numOfAccounts, int accountNumber) {
+    private static int findExistingAccount(int[] accountNumbers, int numOfAccounts, int accountNumber) {
         for (int i = 0; i < numOfAccounts; i++) {
             if (accountNumbers[i] == accountNumber) {
                 return i;
             }
         }
         return -1;
+    }
+
+    private static int findEmptyIndex(int[] accountNumbers, int numOfAccounts) {
+        for (int i = 0; i < numOfAccounts; i++) {
+            if (accountNumbers[i] == 0) {
+                return -1;
+            }
+        }
+        return 0;
+    }
+
+    private static int getValidAccountNumber(Scanner scanner) {
+        System.out.println(ANSI_YELLOW + "Enter the 6-digit account number [100000 to 999999]: " + ANSI_RESET);
+        int accountNumber = scanner.nextInt();
+
+        while(accountNumber < 100000 || accountNumber > 999999) {
+            System.out.println(ANSI_RED + "ERROR: Invalid input. Try again: " + ANSI_RESET);
+            accountNumber = scanner.nextInt();
+        }
+        return accountNumber;
     }
 
     /**
@@ -158,63 +165,76 @@ public class Main {
      */
     private static void withdraw(int[] accountNumbers, double[] accountBalances, int numOfAccounts,
                                    Scanner scanner) {
-        
+        double amountToWithdraw;
+        int accountNumber = getValidAccountNumber(scanner);
+        int accountIndex = findExistingAccount(accountNumbers, numOfAccounts, accountNumber);
+
         System.out.println(ANSI_YELLOW + "\nEnter withdrawal amount: " + ANSI_RESET);
-        double amountToWithdraw = scanner.nextDouble();
+        amountToWithdraw = scanner.nextDouble();
+
+        while (amountToWithdraw <= 0.00) {
+            System.out.println(ANSI_RED + "ERROR: Withdrawal amount must be $0.01 or greater!" + ANSI_RESET);
+            System.out.println(ANSI_YELLOW + "Try again. Enter withdrawal amount: " + ANSI_RESET);
+            amountToWithdraw = scanner.nextDouble();
+        }
+
+        System.out.println("==============================");
+        System.out.println("TRANSACTION TYPE: WITHDRAW");
+        System.out.println("ACCOUNT NUMBER: " + accountNumber);
+        System.out.printf("CURRENT BALANCE: $%.2f", accountBalances[accountIndex]);
+        accountBalances[accountIndex] -= amountToWithdraw;
+        System.out.printf("\nAMOUNT TO WITHDRAW: $%.2f", amountToWithdraw);
+        System.out.printf("\nNEW BALANCE: $%.2f\n", accountBalances[accountIndex]);
+        System.out.println("==============================");
+        returnMsg();
     }
 
     private static void deposit(int[] accountNumbers, double[] accountBalances, int numOfAccounts,
                                 Scanner scanner) {
+        System.out.println(ANSI_YELLOW + "Enter the account number: " + ANSI_RESET);
+        int accountNumber = scanner.nextInt();
+        int accountIndex = findExistingAccount(accountNumbers, numOfAccounts, accountNumber);
+
         System.out.println(ANSI_YELLOW + "Enter deposit amount: " + ANSI_RESET);
         double amountToDeposit = scanner.nextDouble();
-        while (amountToDeposit <= 0.00) {
-            System.out.println(ANSI_BLUE + "\n==============================" + ANSI_RESET);
-            System.out.println(ANSI_YELLOW + "Transaction Type: DEPOSIT");
-            System.out.println("Account Number: " + accountNumber);
-            System.out.printf("Current Balance: $%.2f", accountBalances[accountIndex]);
-            System.out.printf("\nAmount to Deposit: " + ANSI_RED + "$%.2f", amountToDeposit);
-            System.out.printf(ANSI_RESET + ANSI_RED +
-                              "\nERROR: Invalid Deposit Amount - Transaction Voided"
-                            + ANSI_RESET);
-            System.out.println(ANSI_BLUE + "\n==============================" + ANSI_RESET);
 
-            System.out.println(ANSI_RED + "\nPlease try again. Enter deposit amount: " + ANSI_RESET);
+        while (amountToDeposit <= 0.00) {
+            System.out.println(ANSI_RED + "ERROR: Deposit amount must be $0.01 or greater!" + ANSI_RESET);
+            System.out.println(ANSI_YELLOW + "Try again. Enter deposit amount: " + ANSI_RESET);
             amountToDeposit = scanner.nextDouble();
         }
 
         accountBalances[accountIndex] += amountToDeposit;
 
-        System.out.println(ANSI_BLUE + "\n==============================" + ANSI_RESET);
-        System.out.println(ANSI_YELLOW + "Transaction Type: DEPOSIT");
-        System.out.println("Account Number: " + accountNumber);
-        System.out.printf("Current Balance: $%.2f", accountBalances[accountIndex]);
-        System.out.printf("\nAmount to Deposit: $%.2f", amountToDeposit);
-        System.out.printf("\nNew Balance: $%.2f", accountBalances[accountIndex]);
-        System.out.println(ANSI_BLUE + "\n==============================" + ANSI_RESET);
+        System.out.println("==============================");
+        System.out.println("TRANSACTION TYPE: DEPOSIT");
+        System.out.println("ACCOUNT NUMBER: " + accountNumber);
+        System.out.printf("\nCURRENT BALANCE: $%.2f", accountBalances[accountIndex]);
+        System.out.printf("\nAMOUNT TO DEPOSIT: $%.2f", amountToDeposit);
+        System.out.printf("\nNEW BALANCE: $%.2f", accountBalances[accountIndex]);
+        System.out.println("==============================");
+        returnMsg();
     }
 
-    private static void showAccountBalance(int[] accountNumbers, double[] accountBalances, int numOfAccounts,
-                                    Scanner scanner) {
-        int accountNumber, accountIndex;
+    /**
+     * Gets the account balance for any given account.
+     * @param accountNumbers An integer array containing customer account numbers.
+     * @param accountBalances A double array containing customer account balances.
+     * @param numOfAccounts Total number of successful accounts read in.
+     * @param scanner Scanner object passed as an argument to get user input.
+     */
+    private static void getAccountBalance(int[] accountNumbers, double[] accountBalances, int numOfAccounts,
+                                          Scanner scanner) {
 
-        System.out.println(ANSI_YELLOW + "Please enter the account number: " + ANSI_RESET);
-        accountNumber = scanner.nextInt();
-        accountIndex = findAccount(accountNumbers, numOfAccounts, accountNumber);
+        int accountNumber = getValidAccountNumber(scanner);
+        int accountIndex = findExistingAccount(accountNumbers, numOfAccounts, accountNumber);
 
-        while (accountIndex == -1) {
-            System.out.println(ANSI_RED + "Account does not exist or the input is invalid. " +
-                               "Please try again: " + ANSI_RESET);
-            accountNumber = scanner.nextInt();
-            accountIndex = findAccount(accountNumbers, numOfAccounts, accountNumber);
-        }
-
-        System.out.println(ANSI_BLUE + "\n==============================" + ANSI_RESET);
-        System.out.println(ANSI_YELLOW + "Transaction Type: View Balance");
+        System.out.println("\n==============================");
+        System.out.println("Transaction Type: VIEW BALANCE");
         System.out.println("Account Number: " + accountNumber);
-        System.out.printf("Current Total Balance: " + ANSI_RESET + ANSI_GREEN +
-                          "$%.2f", accountBalances[accountIndex]);
-        System.out.println(ANSI_RESET + ANSI_BLUE + "\n==============================" + ANSI_RESET + "\n");
-        System.out.println(ANSI_YELLOW + "Returning to main menu...\n" + ANSI_RESET);
+        System.out.printf("Current Total Balance: $%.2f", accountBalances[accountIndex]);
+        System.out.println("\n==============================");
+        returnMsg();
     }
 
     private static int createNewAccount(int[] accountNumbers, double[] accountBalances, int numOfAccounts,
@@ -224,16 +244,8 @@ public class Main {
         char answer;
 
         do {
-            System.out.println(ANSI_YELLOW + "Please enter a 6-digit non-existing account number " +
-                                             "ranging from 100,000 to 999,999: "
-                             + ANSI_RESET);
-            newAccountNumber = scanner.nextInt();
-
-            while(newAccountNumber < 100000 || newAccountNumber > 999999) {
-                System.out.println(ANSI_RED + "ERROR: Invalid input. Please try again: " + ANSI_RESET);
-                newAccountNumber = scanner.nextInt();
-            }
-            accountIndex = findAccount(accountNumbers,numOfAccounts, newAccountNumber);
+            newAccountNumber = getValidAccountNumber(scanner);
+            accountIndex = findEmptyIndex(accountNumbers,numOfAccounts);
 
             // account can be created
             if (accountIndex == -1) {
@@ -255,6 +267,7 @@ public class Main {
             System.out.println(ANSI_YELLOW + "Create another account? [Y/n]: " + ANSI_RESET);
             answer = scanner.next().charAt(0);
 
+            // checking that the number of accounts does not go over the accountNumbers array length
             if (newNumOfAccounts == accountNumbers.length) {
                 System.out.println(ANSI_RED + "ERROR: Account limit reached!");
                 System.out.println("To create a new account, delete an existing one.");
@@ -262,13 +275,14 @@ public class Main {
                 break;
             }
         } while(answer != 'n' && answer != 'N');
+        returnMsg();
 
         return newNumOfAccounts;
     }
     
     /**
-     * Prompts the user for an account number. If the account number exists and if its balance is
-     * $0.00, then it will get deleted.
+     * Prompts the user for an account number. The account can be deleted if the account number exists and if
+     * its balance is $0.00.
      * @param accountNumbers An integer array that contains customer account numbers.
      * @param accountBalances A double array that contains customer account balances.
      * @param numOfAccounts The total number of successful accounts read in.
@@ -282,31 +296,28 @@ public class Main {
         char userChoice;
 
         do {
-            System.out.println("Please enter the account number: ");
-            accountNumberToDelete = scanner.nextInt();
-            accountIndex = findAccount(accountNumbers, numOfAccounts, accountNumberToDelete);
-
-            while (accountIndex == -1) {
-                System.out.println(ANSI_RED + "ERROR: That account does not exist. Try again:" + ANSI_RESET);
-                accountNumberToDelete = scanner.nextInt();
-                accountIndex = findAccount(accountNumbers, numOfAccounts, accountNumberToDelete);
-            }
+            accountNumberToDelete = getValidAccountNumber(scanner);
+            accountIndex = findExistingAccount(accountNumbers, numOfAccounts, accountNumberToDelete);
 
             if (accountBalances[accountIndex] != 0.00) {
-                System.out.println(ANSI_RED + "ERROR: The account has a balance that is not 0." +
-                                              " - Transaction Voided" + ANSI_RESET);
+                System.out.println(ANSI_RED + "ERROR: The account has a non-zero balance." +
+                                              " Transaction voided."
+                                 + ANSI_RESET);
             } else {
                 accountNumbers[accountIndex] = -1;
                 accountBalances[accountIndex] = -1.00;
                 totalNumberOfAccounts--;
-                System.out.println(ANSI_GREEN + "Account successfully deleted." + ANSI_RESET);
+                System.out.println(ANSI_GREEN + "Account " + accountNumberToDelete + " successfully deleted." +
+                                 " Transaction details sent to output file."
+                                 + ANSI_RESET);
             }
 
             System.out.println(ANSI_YELLOW + "Delete another account? [Y/N]: " + ANSI_RESET);
             userChoice = scanner.next().charAt(0);
         } while(userChoice != 'N' && userChoice != 'n');
 
-        System.out.println(ANSI_YELLOW + "Returning to main menu..." + ANSI_RESET);
+        returnMsg();
+
         return totalNumberOfAccounts;
     }
 
@@ -325,29 +336,12 @@ public class Main {
         System.out.print(ANSI_RESET);
     }
 
-    private static int[] getValidAccount(int[] accountNumbers, int numOfAccounts, Scanner scanner) {
-        int accountNumber = getValidAccountNumber(scanner);
-        int accountIndex = findAccount(accountNumbers,numOfAccounts, accountNumber);
-
-        while (accountIndex == -1) {
-            System.out.println(ANSI_RED + "Account not found. Try again: " + ANSI_RESET);
-            accountNumber = getValidAccountNumber(scanner);
-            accountIndex = findAccount(accountNumbers, numOfAccounts, accountNumber);
+    private static void returnMsg() {
+        System.out.println(ANSI_YELLOW + "Returning to main menu..." + ANSI_RESET);
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
-        return new int[] {accountNumber, accountIndex};
-    }
-
-    private static int getValidAccountNumber(Scanner scanner) {
-        System.out.println(ANSI_YELLOW + "Please enter a 6-digit account number" +
-                                         " in the range [100,000 - 999,999]: " +
-                           ANSI_RESET);
-        int accountNumber = scanner.nextInt();
-
-        while(accountNumber < 100000 || accountNumber > 999999) {
-            System.out.println(ANSI_RED + "ERROR: Invalid input. Try again: " +
-                    ANSI_RESET);
-            accountNumber = scanner.nextInt();
-        }
-        return accountNumber;
     }
 }
